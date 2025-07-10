330aae2a0b3411c7b0c74625a57f34819d23da15
Illya <illamatviuk@gmail.com>
2025-07-10 16:59:00 +0300

    Added GetHistoricalPricesQueryHandler.

---
+++ src/MarketPrice.Application/Queries/GetHistoricalPrices/GetHistoricalPricesQuery.cs
@@ -0,0 +1,6 @@
+﻿using MarketPrice.Application.DTOs.Bar;
+using MediatR;
+
+namespace MarketPrice.Application.Queries.GetHistoricalPrices {
+    public record GetHistoricalPricesQuery(string instrumentId, DateTime startDate, string provider = "oanda") : IRequest<List<BarDto>>;
+}
---
+++ src/MarketPrice.Application/Queries/GetHistoricalPrices/GetHistoricalPricesQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MarketPrice.Application.DTOs.Bar;
+using MarketPrice.Infrastructure.Services.Interfaces;
+using MediatR;
+
+namespace MarketPrice.Application.Queries.GetHistoricalPrices {
+    public class GetHistoricalPricesQueryHandler(
+        IFintachartsService service,
+        IMapper mapper
+        ) : IRequestHandler<GetHistoricalPricesQuery, List<BarDto>> {
+        public async Task<List<BarDto>> Handle(GetHistoricalPricesQuery request, CancellationToken cancellationToken) {
+            var bars = await service.GetHistoricalPricesAsync(request.instrumentId, request.startDate, request.provider);
+            return mapper.Map<List<BarDto>>(bars);
+        }
+    }
+}
