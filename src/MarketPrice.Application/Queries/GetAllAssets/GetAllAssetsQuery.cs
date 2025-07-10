6d335b9e945cc74f0858b9e57b8207ff85f2c123
Illya <illamatviuk@gmail.com>
2025-07-10 15:43:43 +0300

    Added GetAllAssetsQueryHandler.

---
+++ src/MarketPrice.Application/Queries/GetAllAssets/GetAllAssetsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MarketPrice.Application.DTOs.Asset;
+using MediatR;
+
+namespace MarketPrice.Application.Queries.GetAllAssets
+{
+    public record GetAllAssetsQuery : IRequest<List<AssetDto>>;
+}
---
+++ src/MarketPrice.Application/Queries/GetAllAssets/GetAllAssetsQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MarketPrice.Application.DTOs.Asset;
+using MarketPrice.Domain.Interfaces;
+using MediatR;
+
+namespace MarketPrice.Application.Queries.GetAllAssets
+{
+    public class GetAllAssetsQueryHandler(
+        IAssetRepository repository,
+        IMapper mapper
+        ) : IRequestHandler<GetAllAssetsQuery, List<AssetDto>> {
+        public async Task<List<AssetDto>> Handle(GetAllAssetsQuery request, CancellationToken cancellationToken) {
+            var assets = await repository.GetAllAsync(cancellationToken);
+            return mapper.Map<List<AssetDto>>(assets);
+        }
+    }
+}
