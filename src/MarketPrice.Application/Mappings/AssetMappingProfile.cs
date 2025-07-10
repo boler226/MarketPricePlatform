4ef04d06045680fe05f6fdeda515f25eb709d42a
Illya <illamatviuk@gmail.com>
2025-07-10 15:39:09 +0300

    Added AssetMappingProfile.

---
+++ src/MarketPrice.Application/Mappings/AssetMappingProfile.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MarketPrice.Application.DTOs.Asset;
+using MarketPrice.Application.DTOs.Bar;
+using MarketPrice.Domain.Entities.Asset;
+using MarketPrice.Domain.Entities.Bar;
+
+namespace MarketPrice.Application.Mappings
+{
+    public class AssetMappingProfile : Profile {
+        public AssetMappingProfile() {
+            CreateMap<AssetEntity, AssetDto>()
+                .ForMember(dest => dest.Mappings, opt => opt.MapFrom(src =>
+                    src.Mappings.ToDictionary(
+                        m => m.Provider,
+                        m => new MappingDto {
+                            Symbol = m.Symbol,
+                            Exchange = m.Exchange,
+                            DefaultOrderSize = m.DefaultOrderSize,
+                            MaxOrderSize = m.MaxOrderSize,
+                            TradingHours = new TradingHoursDto {
+                                RegularStart = m.RegularStart,
+                                RegularEnd = m.RegularEnd,
+                                ElectronicStart = m.ElectronicStart,
+                                ElectronicEnd = m.ElectronicEnd
+                            }
+                        })))
+                .ForMember(dest => dest.Profile, opt => opt.MapFrom(src => src.Profile));
+
+            CreateMap<AssetDto, AssetEntity>()
+               .ForMember(dest => dest.Mappings, opt => opt.Ignore())
+               .ForMember(dest => dest.Profile, opt => opt.MapFrom(src => src.Profile));
+
+            CreateMap<ProfileEntity, ProfileDto>();
+            CreateMap<ProfileDto, ProfileEntity>();
+
+            CreateMap<MappingEntity, MappingDto>();
+            CreateMap<MappingDto, MappingEntity>();
+
+            CreateMap<BarEntity, BarDto>();
+            CreateMap<BarDto, BarEntity>();
+        }
+    }
+}
