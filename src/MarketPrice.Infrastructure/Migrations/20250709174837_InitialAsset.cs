41ea6533fc44b76eb37f956dfa51438ee0531130
Illya <illamatviuk@gmail.com>
2025-07-10 15:41:37 +0300

    Added InitialAsset migration.

---
+++ src/MarketPrice.Infrastructure/Migrations/20250709174837_InitialAsset.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketPrice.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialAsset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Symbol = table.Column<string>(type: "text", nullable: false),
+                    Kind = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    TickSize = table.Column<decimal>(type: "numeric", nullable: false),
+                    Currency = table.Column<string>(type: "text", nullable: false),
+                    BaseCurrency = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bars",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Open = table.Column<decimal>(type: "numeric", nullable: false),
+                    High = table.Column<decimal>(type: "numeric", nullable: false),
+                    Low = table.Column<decimal>(type: "numeric", nullable: false),
+                    Close = table.Column<decimal>(type: "numeric", nullable: false),
+                    Volume = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mappings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Provider = table.Column<string>(type: "text", nullable: false),
+                    Symbol = table.Column<string>(type: "text", nullable: false),
+                    Exchange = table.Column<string>(type: "text", nullable: false),
+                    DefaultOrderSize = table.Column<int>(type: "integer", nullable: false),
+                    MaxOrderSize = table.Column<int>(type: "integer", nullable: true),
+                    RegularStart = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    RegularEnd = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    ElectronicStart = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    ElectronicEnd = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    AssetId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mappings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mappings_Assets_AssetId",
+                        column: x => x.AssetId,
+                        principalTable: "Assets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    AssetId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profiles_Assets_AssetId",
+                        column: x => x.AssetId,
+                        principalTable: "Assets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mappings_AssetId",
+                table: "Mappings",
+                column: "AssetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_AssetId",
+                table: "Profiles",
+                column: "AssetId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bars");
+
+            migrationBuilder.DropTable(
+                name: "Mappings");
+
+            migrationBuilder.DropTable(
+                name: "Profiles");
+
+            migrationBuilder.DropTable(
+                name: "Assets");
+        }
+    }
+}
