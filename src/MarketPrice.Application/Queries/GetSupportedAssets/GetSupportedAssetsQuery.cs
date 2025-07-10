0681262914d5463567500980694bf19d27066463
Illya <illamatviuk@gmail.com>
2025-07-10 15:43:56 +0300

    Added GetSupportedAssetsQueryHandler.

---
+++ src/MarketPrice.Application/Queries/GetSupportedAssets/GetSupportedAssetsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MarketPrice.Application.DTOs.Asset;
+using MediatR;
+
+namespace MarketPrice.Application.Queries.GetSupportedAssets
+{
+    public record GetSupportedAssetsQuery : IRequest<List<AssetDto>>;
+}
---
+++ src/MarketPrice.Application/Queries/GetSupportedAssets/GetSupportedAssetsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MarketPrice.Application.DTOs.Asset;
+using MarketPrice.Domain.Entities;
+using MarketPrice.Domain.Interfaces;
+using MarketPrice.Infrastructure.Services.Interfaces;
+using MediatR;
+
+namespace MarketPrice.Application.Queries.GetSupportedAssets
+{
+    public class GetSupportedAssetsQueryHandler(
+        IFintachartsService service,
+        IMapper mapper
+        ) : IRequestHandler<GetSupportedAssetsQuery, List<AssetDto>> {
+        public async Task<List<AssetDto>> Handle(GetSupportedAssetsQuery request, CancellationToken cancellationToken) {
+            var assetDtos = await service.GetSupportedAssetsAsync();
+            return mapper.Map<List<AssetDto>>(assetDtos);
+        }
+    }
+}
