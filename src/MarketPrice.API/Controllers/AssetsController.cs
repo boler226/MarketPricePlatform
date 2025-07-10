e05f0391fb9686f40e7d70686dc6e0f1f1969158
Illya <illamatviuk@gmail.com>
2025-07-10 16:59:25 +0300

    Added GetHistoricalPrices for AssetsController.

--- src/MarketPrice.API/Controllers/AssetsController.cs
+++ src/MarketPrice.API/Controllers/AssetsController.cs
@@ -1,6 +1,8 @@
 ﻿using MarketPrice.Application.Commands.SyncSupportedAssets;
 using MarketPrice.Application.DTOs.Asset;
+using MarketPrice.Application.DTOs.Bar;
 using MarketPrice.Application.Queries.GetAllAssets;
+using MarketPrice.Application.Queries.GetHistoricalPrices;
 using MarketPrice.Application.Queries.GetSupportedAssets;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +21,12 @@ namespace MarketPrice.API.Controllers
         [HttpGet("supported")]
         public async Task<ActionResult<List<AssetDto>>> GetSupportedAssets() {
             var result = await mediator.Send(new GetSupportedAssetsQuery());
+            return result;
+        }
+
+        [HttpGet("historical")]
+        public async Task<ActionResult<List<BarDto>>> GetHistoricalPrices(string instrumentId, DateTime startDate, string provider = "oanda") {
+            var result = await mediator.Send(new GetHistoricalPricesQuery(instrumentId, startDate, provider));
             return result;
         }
 
